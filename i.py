04f37a4398116b0dcd4009a3bc98078ebab0f91e
Tanmay04032002 <tanmayaggarwal403@gmail.com>
2026-03-27 15:25:37 +0530

    Update i.py

--- i.py
+++ i.py
@@ -14,7 +14,7 @@ os.makedirs(download_folder, exist_ok=True)
 
 base_url = "https://ibfd.archivalware.co.uk/awweb/pdfopener?md=1&did="
 
-start = 68500
+start = 64000
 end   = 45001
 
 MAX_WORKERS = 1          # 2 browsers open at all times throughout the run
